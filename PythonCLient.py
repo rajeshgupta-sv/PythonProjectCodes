a303433354c94fdb163a23ef1206907c4beaa02e
rajeshgupta-sv <46725454+rajeshgupta-sv@users.noreply.github.com>
2019-10-02 09:59:32 -0500

    Added Python Codes from Exercise

---
+++ PythonCLient.py
@@ -0,0 +1,26 @@
+import requests
+
+resp = requests.get('http://127.0.0.1:5000')
+
+print (resp.status_code)
+print(resp.text)
+
+resp = requests.get('http://127.0.0.1:5000/greet')
+
+print (resp.status_code)
+print(resp.text)
+
+resp = requests.get('http://127.0.0.1:5000/get_time')
+
+print (resp.status_code)
+print(resp.text)
+
+resp = requests.get('http://127.0.0.1:5000/get_value')
+
+print (resp.status_code)
+print(resp.text)
+
+resp = requests.get('http://127.0.0.1:5000/bye')
+
+print (resp.status_code)
+print(resp.text)
